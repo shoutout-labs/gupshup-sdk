bf435cff61677378a1a5d0f8a6da358cd1d13b6e
madura <madura@sqrmobile.com>
2021-06-30 15:46:04 +0530

    update apikey

--- test/test_gupshup.rb
+++ test/test_gupshup.rb
@@ -3,7 +3,7 @@ require 'gupshup'
 
 class GupshupSdkTest < Minitest::Test
     def setup
-        @gupshup = Gupshup::Client.new("ma7iaeu7htm6mezzfsbxezmjrwkqof7q")
+        @gupshup = Gupshup::Client.new("APIKEY")
     end
     def test_send_whatsApp
         response=@gupshup.sendWhatsApp(from:"917834811114",to:"94778845713",body:"This is to remind you that {{1}} is due by {{2}}.",appName:"Shoutouttest")
